a33a3281abe08d4cb54b66f6b3db9a172dce2934
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-06-11 13:11:32 -0400

    index task 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+//1. Add in all necessary components and libary methods.
 import React from "react";
 import ReactDOM from "react-dom";
-
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./App";
+import { reducer } from './reducers';
 
 const { worker } = require('./mocks/browser');
 worker.start();
@@ -15,6 +20,5 @@ ReactDOM.render(
 );
 
 //Task List:
-//1. Add in all necessary components and libary methods.
 //2. Create a store that includes thunk and logger middleware support.
 //3. Wrap the App component in a react-redux Provider element.
