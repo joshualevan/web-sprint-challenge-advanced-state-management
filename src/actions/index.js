676ca9e22dbfce60f53120ff83426498163068ef
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-06-11 14:34:51 -0400

    Q 1-2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+export const fetchSmurfs = () => {
+    return (dispatch => {
+        dispatch({type: SMURF_FETCH});
+    
+        dispatch(smurfFetch());
+        axios.get('http://localhost:3000/')
+          .then(response => {
+            dispatch({type: FETCH_SUCCEEDS, payload:response.data});
+          })
+          .catch(error =>{
+            dispatch({type: FETCH_FAILS, payload:error});
+          })
+    });
+}
+
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
